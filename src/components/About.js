6db639c4328f9e09e82024c64157b89c768c5581
Deeksha <deekshad1502@gmail.com>
2024-03-14 21:54:23 +0530

    about component modified

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,7 @@
-import { UserClass } from "./UserClass";
-
 export const About = () => {
   return (
     <div>
       <h1>About</h1>
-
-      <UserClass name={"first"} location={"Bangalore"} />
     </div>
   );
 };
