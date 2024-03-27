b86c7124f5e00afd4ac4731be3271c2eb79d65a4
Deeksha <deekshad1502@gmail.com>
2024-03-27 11:58:48 +0530

    added styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,31 +12,29 @@ export const Header = () => {
   const { setFilteredRestaurant } = useProducts();
 
   return (
-    <div className="flex justify-between bg-blue-700 shadow-md h-20">
+    <div className="flex sticky top-0 bg-opacity-100 justify-between bg-slate-50 shadow-md h-24 text-lg mb-9 ">
       <div className="logo-container">
         <Link to="/">
           <div onClick={() => setFilteredRestaurant(resList)}>
-            <img className="w-20 h-50" src={LOGO_URL} />
+            <img className="w-24 max-w-xs h-auto" src={LOGO_URL} />
           </div>
         </Link>
       </div>
       <div className="flex items-center ">
-        <ul className="sticky text-white flex p-4 m-4 font-sans">
-          <li className="px-4 font-semibold">
-            Online Status: {onlineStatus ? "🟢" : "🔴"}
-          </li>
-          <li className="px-4 font-semibold">
+        <ul className="sticky text-black flex p-4 m-4 font-serif">
+          <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4 ">
             <Link to="/">Home</Link>
           </li>
-          <li className="px-4 font-semibold">
+          <li className="px-4 ">
             <Link to="/about">About Us</Link>
           </li>
-          <li className="px-4 font-semibold">
+          <li className="px-4 ">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li className="px-4 font-semibold">Cart</li>
+          <li className="px-4 ">Cart</li>
           <button
-            className="login px-4 font-semibold"
+            className="login px-4 "
             onClick={() => {
               btnNameReact == "Login"
                 ? setBtnNameReact("Logout")
