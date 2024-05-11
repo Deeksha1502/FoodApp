e5dfc55cff360555fffa5f640f152a88170858a8
Deeksha <deekshad1502@gmail.com>
2024-05-11 23:51:44 +0530

    changed styles for buttons in the home page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
 import { CartProvider } from "./context/ContextItems.js";
 import { appRouter } from "./Router.js";
+import { Provider } from "react-redux";
+import { appStore } from "./utils/appStore.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={appStore}>
   <CartProvider>
     <RouterProvider router={appRouter} />
-  </CartProvider>,
+  </CartProvider>
+  </Provider>
 );
