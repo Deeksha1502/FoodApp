d010acce1b9aa81caa74a4e9a0080eb260f6b838
Deeksha <deekshad1502@gmail.com>
2024-06-05 23:26:13 +0530

    feat/contact

--- src/components/RestaurantMenuList.js
+++ src/components/RestaurantMenuList.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import { TfiRulerAlt } from "react-icons/tfi";
+
 export const RestaurantMenuList = (props) => {
   const { resMenu } = props;
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
+  const [showItems, setShowItems] = useState(false);
+
   console.log(resMenu);
   return (
     <div>
@@ -7,68 +15,112 @@ export const RestaurantMenuList = (props) => {
         <ul>
           {resMenu.map((item) => (
             <li key={item.id}>
-              <div className="w-6/12 mx-auto my-4 shadow-lg p-4 bg-gray-50 ">
-                <h2 className="text-3xl my-6 font-bold">
-                  {item.restaurantName}
-                </h2>
-                <h4 className="font-bold">{item.firstItem}</h4>
-                <p className="font-bold">{item.firstDescription}</p>
-                <p>Cost: {item.firstItemCost}</p>
-
-                {item.firstCloudinaryimageid && (
-                  <img
-                    src={item.firstCloudinaryimageid}
-                    alt={item.firstItem}
-                    className="flex justify-center items-center"
-                  />
-                )}
+              <span className="text-3xl my-6 font-bold ">
+                {item.restaurantName}
+              </span>
+              <div
+                className="flex flex-col-2 justify-between py-6 cursor-pointer"
+                onClick={handleClick}
+              >
+                <span className="flex justify-between font-bold text-xl">
+                  Recommended
+                </span>
+                <span>⬇</span>
               </div>
-              <div className="w-6/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
-                <h4 className="font-bold">{item.secondItem}</h4>
-                <p className="font-bold">{item.secondDescription}</p>
-                <p>Cost: {item.secondItemCost}</p>
+              {showItems && (
+                <div>
+                  <div className="w-9/12 mx-auto my-4 shadow-lg p-4 bg-gray-50 ">
+                    <h4 className="font-bold">{item.firstItem}</h4>
+                    <p className="font-bold">{item.firstDescription}</p>
+                    <p>Cost: {item.firstItemCost}</p>
 
-                {item.secondCloudinaryimageid && (
-                  <img
-                    src={item.secondCloudinaryimageid}
-                    alt={item.secondItem}
-                  />
-                )}
-              </div>
-              <div className="w-6/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
-                <h4 className="font-bold">{item.thirdItem}</h4>
-                <p className="font-bold">{item.thirdDescription}</p>
-                <p>Cost: {item.thirdItemCost}</p>
+                    <div className="absolute">
+                      <button className="p-2 bg-slate-200  m-auto rounded-lg">
+                        Add +
+                      </button>
+                    </div>
 
-                {item.thirdCloudinaryimageid && (
-                  <img src={item.thirdCloudinaryimageid} alt={item.thirdItem} />
-                )}
-              </div>
-              <div className="w-6/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
-                <h4 className="font-bold">{item.fourthItem}</h4>
-                <p className="font-bold">{item.fourthDescription}</p>
-                <p>Cost: {item.fourthItemCost}</p>
+                    {item.firstCloudinaryimageid && (
+                      <img
+                        src={item.firstCloudinaryimageid}
+                        alt={item.firstItem}
+                        className="flex justify-center items-center"
+                      />
+                    )}
+                  </div>
+                  <div className="w-9/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
+                    <h4 className="font-bold">{item.secondItem}</h4>
+                    <p className="font-bold">{item.secondDescription}</p>
+                    <p>Cost: {item.secondItemCost}</p>
 
-                {item.fourthCloudinaryimageid && (
-                  <img
-                    src={item.fourthCloudinaryimageid}
-                    alt={item.fourthItem}
-                  />
-                )}
-              </div>
-              <div className="w-6/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
-                <h4 className="font-bold">{item.fifthItem}</h4>
-                <p className="font-bold">{item.fifthDescription}</p>
-                <p>Cost: {item.fifthItemCost}</p>
+                    <div className="absolute">
+                      <button className="p-2 bg-slate-200  m-auto rounded-lg">
+                        Add +
+                      </button>
+                    </div>
 
-                {item.fifthCloudinaryimageid && (
-                  <img
-                    src={item.fifthCloudinaryimageid}
-                    alt={item.fifthItem}
-                    className="flex justify-center items-center"
-                  />
-                )}
-              </div>
+                    {item.secondCloudinaryimageid && (
+                      <img
+                        src={item.secondCloudinaryimageid}
+                        alt={item.secondItem}
+                      />
+                    )}
+                  </div>
+                  <div className="w-9/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
+                    <h4 className="font-bold">{item.thirdItem}</h4>
+                    <p className="font-bold">{item.thirdDescription}</p>
+                    <p>Cost: {item.thirdItemCost}</p>
+                    <div className="absolute">
+                      <button className="p-2 bg-slate-200  m-auto rounded-lg">
+                        Add +
+                      </button>
+                    </div>
+
+                    {item.thirdCloudinaryimageid && (
+                      <img
+                        src={item.thirdCloudinaryimageid}
+                        alt={item.thirdItem}
+                      />
+                    )}
+                  </div>
+                  <div className="w-9/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
+                    <h4 className="font-bold">{item.fourthItem}</h4>
+                    <p className="font-bold">{item.fourthDescription}</p>
+                    <p>Cost: {item.fourthItemCost}</p>
+                    <div className="absolute">
+                      <button className="p-2 bg-slate-200  m-auto rounded-lg">
+                        Add +
+                      </button>
+                    </div>
+
+                    {item.fourthCloudinaryimageid && (
+                      <img
+                        src={item.fourthCloudinaryimageid}
+                        alt={item.fourthItem}
+                      />
+                    )}
+                  </div>
+                  <div className="w-9/12 mx-auto my-4 shadow-lg p-4 bg-gray-50">
+                    <h4 className="font-bold">{item.fifthItem}</h4>
+                    <p className="font-bold">{item.fifthDescription}</p>
+                    <p>Cost: {item.fifthItemCost}</p>
+                    <div className="absolute">
+                      <button className="p-2 bg-slate-200 shadow-lg mx-16 m-auto rounded-lg">
+                        Add +
+                      </button>
+                    </div>
+
+                    {item.fifthCloudinaryimageid && (
+                      <img
+                        src={item.fifthCloudinaryimageid}
+                        alt={item.fifthItem}
+                        className="flex justify-center items-center"
+                      />
+                    )}
+                  </div>
+                  <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                </div>
+              )}
             </li>
           ))}
         </ul>
