7aea994e2ae8be345f7d89825d8330db14059414
Deeksha <deekshad1502@gmail.com>
2024-06-07 22:02:39 +0530

    feat/cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,11 @@
+import { useSelector } from "react-redux"
+
 export const Cart = () =>{
-    return <div className="min-h-screen bg-gradient-to-r from-violet-200 to-pink-200"> Coming soon! </div>
+
+const cartItems = useSelector((store) =>store.cart.items)
+
+    return <div className="min-h-screen bg-gradient-to-r from-violet-200 to-pink-200"> 
+    <div className="font-bold text-center text-2xl mt-3">
+        Cart
+    </div> </div>
 }
